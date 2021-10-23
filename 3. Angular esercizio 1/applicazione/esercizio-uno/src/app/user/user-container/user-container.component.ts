360e03a628b08a8a6575b610465012545407f471
Mateusz Rodz <50263944+Mate-usz@users.noreply.github.com>
2021-10-23 23:02:56 +0200

    feat: Angular esercizio 1

---
+++ 3. Angular esercizio 1/applicazione/esercizio-uno/src/app/user/user-container/user-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'es-user-container',
+  templateUrl: './user-container.component.html',
+  styleUrls: ['./user-container.component.css']
+})
+export class UserContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
